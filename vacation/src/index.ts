0651388b9bfcbfad052a5b1870e2e5c7263e00c2
Artom Sopun <artomsopun@gmail.com>
2022-05-06 23:18:14 +0300

    added dotenv, parsers, async write

--- vacation/src/index.ts
+++ vacation/src/index.ts
@@ -1,6 +1,15 @@
 import fs from "fs";
 import path from "path";
-//require('dotenv').config();
+require('dotenv').config();
+
+class Configs {
+    input: string;
+    output: string;
+    constructor() {
+        this.input = process.env.INPUT_JSON || "input.json";
+        this.output = process.env.OUTPUT_JSON || "output.json";
+    }
+}
 
 interface UserJSON {
     _id: string;
@@ -9,7 +18,7 @@ interface UserJSON {
 
 interface WeekendDateJSON {
     _id: string;
-    user: User;
+    user: UserJSON;
     usedDays: number;
     startDate: string;
     endDate: string;
@@ -36,22 +45,58 @@ class WeekendDate {
     }
 }
 
-const readFile = (path: string) => {
-    return new Promise<string>((resolve, reject) => fs.readFile(path, { encoding: "utf8" }, (err, data: string) => {
-        if (err) {
-            return reject(err);
-        }
-        resolve(data);
-    }))
+const readFileAsync = (file: string) => {
+    return new Promise<string>((resolve, reject) => fs.readFile(path.resolve(__dirname, file),
+        { encoding: "utf8" }, (err, data: string) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        }))
 }
 
+const writeFileAsync = (file: string, data: string) => {
+    return new Promise((reject) => fs.writeFile(path.resolve(__dirname, file),
+        data, (err) => {
+            if (err) {
+                return reject(err);
+            }
+        }))
+}
+
+const getUniqueList = (arr: WeekendDateJSON[]) => {
+    let users: User[] = [];
+
+    arr.forEach(val => {
+        let user = users.find(item => item.userId == val.user._id);
+        if (!user) {
+            user = new User(val.user._id, val.user.name)
+            users.push(user);
+        }
+        user.weekendDates.push(new WeekendDate(val.startDate, val.endDate));
+    })
+
+    return users;
+}
+
+const printList = (users: User[]) => {
+    users.forEach(user => {
+        console.log(`${user.userId} ${user.name}:`);
+        user.weekendDates.forEach(date => {
+            console.log(`  { ${date.startDate} — ${date.endDate} }`);
+        })
+        console.log()
+    })
+    return users;
+}
 
 const init = () => {
-    readFile(path.resolve(__dirname, "data.json")).then((data: string) => {
-        let weks: WeekendDateJSON[] = [];
-        weks = JSON.parse(data);
-        console.log(weks);
-    } );
+    const cfg = new Configs();
+    readFileAsync(cfg.input)
+        .then((data: string) => JSON.parse(data) as WeekendDateJSON[])
+        .then((data: WeekendDateJSON[]) => getUniqueList(data))
+        .then((data: User[]) => printList(data))
+        .then((data: User[]) => writeFileAsync(cfg.output, JSON.stringify(data, null, "\t")))
 }
 
 init();
