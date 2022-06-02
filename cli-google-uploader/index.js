f5299b50b01293a0359e7a0f5aab4ea84844bb69
Artom Sopun <artomsopun@gmail.com>
2022-06-02 07:10:12 +0300

    added uploader, link shortener

--- cli-google-uploader/index.js
+++ cli-google-uploader/index.js
@@ -1,8 +1,7 @@
-//cli-google-uploader@peerless-clock-352021.iam.gserviceaccount.com
-
 const { google } = require("googleapis");
-const path = require("path");
+const inquirer = require("inquirer");
 const fs = require("fs");
+const prettylink = require("prettylink");
 
 const CLIENT_ID =
   "324933358487-jg9ate8u0ut8bl1al6ijhd534o86lsr1.apps.googleusercontent.com";
@@ -25,23 +24,91 @@ const drive = google.drive({
   auth: oauth2Client,
 });
 
-const filePath = path.resolve(__dirname, "tower.jpg");
+const uploadFile = async (fileName, filePath) => {
+  const response = await drive.files.create({
+    requestBody: {
+      name: fileName,
+      parents: ["1TK0XIFfyNP0rVNYK63OFWAW_7rGwsuu1"],
+    },
+    media: {
+      body: fs.createReadStream(filePath),
+    },
+  });
+  return response.data;
+};
 
-const uploadFile = async () => {
+const getPathQ = {
+  type: "input",
+  message:
+    "Drag and drop your image to terminal and press ENTER for uploading:",
+  name: "path",
+};
+
+const changeTitleQ = (title) => {
+  return {
+    type: "confirm",
+    message: `You upload the file with the name: ${title}. Would you like to change it?`,
+    name: "isChange",
+  };
+};
+
+const enterTitleQ = {
+  type: "input",
+  message: "Enter new file name (without extension .jpg, .png, etc.):",
+  name: "title",
+};
+
+const shortUrlQ = {
+  type: "confirm",
+  message: `Would you like to shorten your link?`,
+  name: "isShort",
+};
+
+const startUpload = async () => {
   try {
-    const response = await drive.files.create({
-      requestBody: {
-        name: "image.jpg",
-        parents: ['1TK0XIFfyNP0rVNYK63OFWAW_7rGwsuu1'],
-      },
-      media: {
-        body: fs.createReadStream(filePath),
-      },
-    });
-    console.log(response.data);
+    const file = await inquirer.prompt(getPathQ);
+    let fileName = file.path.split("\\").pop();
+    const ext = "." + fileName.split(".").pop();
+    console.log(
+      `Path to file: ${file.path}\nFile name: ${fileName}\nFile extension: ${ext}`
+    );
+    const confirm = await inquirer.prompt(changeTitleQ(fileName));
+    if (confirm.isChange) {
+      const input = await inquirer.prompt(enterTitleQ);
+      fileName = input.title + ext;
+      console.log({ newFileName: fileName });
+    }
+    const obj = await uploadFile(fileName, file.path);
+    console.log(`${file.path}\nSuccessfully uploaded!`);
+    const confirmUrl = await inquirer.prompt(shortUrlQ);
+    const link = (await generatePublicUrl(obj.id)).webViewLink;
+    if (confirmUrl.isShort) {
+      console.log("Your short link:", await getShortUrl(link));
+    } else {
+      console.log("Your link:", link);
+    }
   } catch (error) {
-      console.log(error.message);
+    console.log(error);
   }
 };
 
-uploadFile();
+const generatePublicUrl = async (fileId) => {
+  await drive.permissions.create({
+    fileId: fileId,
+    requestBody: {
+      role: "reader",
+      type: "anyone",
+    },
+  });
+  const result = await drive.files.get({
+    fileId: fileId,
+    fields: "webViewLink", //webContentLink
+  });
+  return result.data;
+};
+
+const getShortUrl = async (url) => {
+  return await new prettylink.TinyURL().short(url);
+};
+
+startUpload();
