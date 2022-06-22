6f01d27d6372959b3a22f6dbedceb0dbc28ea84b
Artom Sopun <artomsopun@gmail.com>
2022-06-22 18:04:08 +0300

    added eslint

---
+++ crypto-commutator/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 0,
+    'no-multi-spaces': ['error'],
+  },
+};
--- crypto-commutator/src/index.ts
+++ crypto-commutator/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello Artom!');
+const a = () => 'Hello';
+console.log(a());
+
+const b = () => {
+  console.log('Ji');
+};
+
+b();
+
+const helloYou = (name: string) => {
+  console.log(`hello${name}!`);
+};
+
+helloYou('hello');
