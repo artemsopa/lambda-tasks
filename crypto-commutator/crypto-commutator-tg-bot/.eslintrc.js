e0ce48de8c2bdd05285aa3ce8f801a6b833fdca0
Artom Sopun <artomsopun@gmail.com>
2022-06-30 17:59:11 +0300

    init commit

---
+++ crypto-commutator/crypto-commutator-tg-bot/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'linebreak-style': 0,
+    'no-console': 0,
+    'no-multi-spaces': ['error'],
+    'consistent-return': 0,
+    'lines-between-class-members': 0,
+    'max-classes-per-file': 0,
+    'object-curly-newline': 0,
+  },
+};
