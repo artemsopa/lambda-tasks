a343642306867056d61d4d99d258dec5606b82c8
Artom Sopun <artomsopun@gmail.com>
2022-07-06 23:07:49 +0300

    updated formatting

--- crypto-commutator/crypto-commutator-tg-bot/src/commutator.ts
+++ crypto-commutator/crypto-commutator-tg-bot/src/commutator.ts
@@ -5,48 +5,12 @@ interface InfoCoin {
   price: number;
 }
 
-const getStr = (str: string, length: number) => {
-  const count = length - str.length;
-  let result = str;
-  for (let i = 0; i < count; i++) {
-    result += '⠀';
-  }
-  return result;
-};
-
 const formatList = (list: InfoCoin[]) => {
   let str = '';
-  for (let i = 0; i < list.length; i += 3) {
-    const line = list.slice(i, i + 3).sort((a, b) => b.price - a.price);
-    for (let j = 0; j < line.length; j++) {
-      const price = line[j].price.toFixed(2);
-      let length = 6;
-      if (price.length > 7 || j === 0) length = 9;
-      str += `🔐${getStr(`/${line[j].name}`, length)}\t `;
-      if (line[j].name.toLowerCase().includes('i')) {
-        str += ' ';
-      }
-    }
-    str += '\n';
-    for (let j = 0; j < line.length; j++) {
-      const price = line[j].price.toFixed(2);
-      let length = 6;
-      if (price.length > 7 || j === 0) length = 9;
-      str += `💸${getStr(`${price}$`, length)}\t  `;
-      if (line[j].price.toFixed(2).includes('1')) {
-        str += ' ';
-      }
-    }
-    str += '\n\n';
+  for (let i = 0; i < list.length; i++) {
+    const price = list[i].price.toFixed(2);
+    str += ` 🔐 /${list[i].name} \t-\t ${price}$\n`;
   }
-  return str;
-};
-
-const formatListFav = (list: InfoCoin[]) => {
-  let str = '';
-  list.forEach((item: InfoCoin) => {
-    str += `⠀🔐 /${item.name} \n⠀💸 ${item.price.toFixed(2)}$\n\n`;
-  });
   return str;
 };
 
@@ -62,17 +26,17 @@ interface CoinPrices {
 
 const getTime = (str: string) => {
   const symbol = str.slice(-1);
-  if (str === '1m') return '1 minute ago:';
-  if (str === '1h') return '1 hour ago:';
-  if (symbol === 'm') return `${str.slice(0, -1)} minutes ago:`;
-  if (symbol === 'h') return `${str.slice(0, -1)} hours ago:`;
+  if (str === '1m') return '1 minute';
+  if (str === '1h') return '1 hour';
+  if (symbol === 'm') return `${str.slice(0, -1)} minutes`;
+  if (symbol === 'h') return `${str.slice(0, -1)} hours`;
   return str;
 };
 
 const formatInfos = (list: CoinPrices) => {
   let str = `🔐 /${list.name} by past 24 hours:\n\n`;
   list.prices.forEach((item: CoinPrice) => {
-    str += ` ⏰ ${getStr(getTime(item.time), 15)} \t 💸 ${item.price.toFixed(2)}$\n\n`;
+    str += ` ⏰ ${getTime(item.time)} ago \t-\t ${item.price.toFixed(2)}$\n\n`;
   });
   return str;
 };
@@ -106,7 +70,7 @@ class Commutator {
     try {
       const response = await axios.get<InfoCoin[]>(`${this.baseUrl}favs/${id}`);
       if (response.data.length === 0) return 'Your favourite list is empty!';
-      return `⭐️ YOUR FAVOURITES\n\n${formatListFav(response.data)}`;
+      return `⭐️ YOUR FAVOURITES\n\n${formatList(response.data)}`;
     } catch (error: any) {
       if (error.response.data) return error.response.data.message;
       return error.message;
