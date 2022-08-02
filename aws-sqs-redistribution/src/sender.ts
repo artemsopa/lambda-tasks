11c1810ae642a5b6356169c510a56a001bf2b24b
Artom Sopun <artomsopun@gmail.com>
2022-08-03 00:52:57 +0300

    updated messages

--- aws-sqs-redistribution/src/sender.ts
+++ aws-sqs-redistribution/src/sender.ts
@@ -6,14 +6,14 @@ const configs = initConfigs();
 const sqs = new AWS.SQS();
 
 const sender: APIGatewayProxyHandler = async (event) => {
-  let statusCode: number = 200;
-  let message: string;
+  let statusCode = 200;
+  let message = 'Message placed in the Queue!';
 
   if (!event.body) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'No body was found',
+        message: 'Body is not found',
       }),
     };
   }
@@ -22,7 +22,7 @@ const sender: APIGatewayProxyHandler = async (event) => {
   const accountId = configs.ACCOUNT_ID;
   const queueName = configs.QUEUE_NAME;
 
-  const queueUrl: string = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
+  const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
 
   try {
     await sqs.sendMessage({
@@ -35,12 +35,10 @@ const sender: APIGatewayProxyHandler = async (event) => {
         },
       },
     }).promise();
-
-    message = 'Message placed in the Queue!';
   } catch (error) {
     console.log(error);
-    message = 'Cannot place message in the Queue!';
     statusCode = 500;
+    message = 'Cannot place message in the Queue!';
   }
 
   return {
