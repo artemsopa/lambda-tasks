f9cb167858b4993c06ac595740e191cc34dbb774
Artom Sopun <artomsopun@gmail.com>
2022-08-03 00:00:56 +0300

    added dotenv

--- aws-sqs-redistribution/src/index.ts
+++ aws-sqs-redistribution/src/index.ts
@@ -1,8 +1,2 @@
-import middy from '@middy/core';
-import sender from './sender';
-import receiver from './receiver';
-
-export default {
-  sender: middy(sender),
-  receiver: middy(receiver),
-} as const;
+export { default as sender } from './sender';
+export { default as receiver } from './receiver';
