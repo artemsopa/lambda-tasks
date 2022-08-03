2e43b128aada19a62aac4a22cc183e061fc7a7fb
Artom Sopun <artomsopun@gmail.com>
2022-08-03 17:17:35 +0300

    updated handler exports

--- aws-image-uploader/src/index.ts
+++ aws-image-uploader/src/index.ts
@@ -2,11 +2,13 @@ import middy from '@middy/core';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import handler from './handler/handler';
 
-export const signIn = middy(handler.auth.signIn.bind(handler.auth))
-  .use(httpJsonBodyParser());
-export const signUp = middy(handler.auth.signUp.bind(handler.auth))
-  .use(httpJsonBodyParser());
+export default {
+  signIn: middy(handler.auth.signIn.bind(handler.auth))
+    .use(httpJsonBodyParser()),
+  signUp: middy(handler.auth.signUp.bind(handler.auth))
+    .use(httpJsonBodyParser()),
 
-export const getAllImages = handler.bucket.getAllImages.bind(handler.bucket);
-export const uploadImage = handler.bucket.uploadImage.bind(handler.bucket);
-export const deleteImage = handler.bucket.deleteImage.bind(handler.bucket);
+  getAllImages: handler.bucket.getAllImages.bind(handler.bucket),
+  uploadImage: handler.bucket.uploadImage.bind(handler.bucket),
+  deleteImage: handler.bucket.deleteImage.bind(handler.bucket),
+};
